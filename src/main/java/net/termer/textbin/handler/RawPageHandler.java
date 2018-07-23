fc566552a2c9f82929eafa455b8f8a1c29987c02
TermerMC <termer.net@gmail.com>
2018-07-23 18:51:55 -0400

    add view post raw handler

---
+++ src/main/java/net/termer/textbin/handler/RawPageHandler.java
@@ -0,0 +1,32 @@
+package net.termer.textbin.handler;
+
+import java.io.File;
+import java.io.IOException;
+
+import net.termer.twister.document.DocumentBuilder;
+import net.termer.twister.handler.RequestHandler;
+import spark.Request;
+import spark.Response;
+
+public class RawPageHandler implements RequestHandler {
+	public String handle(Request req, Response res) {
+		String r = "Unknown post ID";
+		
+		if(req.queryParams("id") != null) {
+			String id = req.queryParams("id");
+			File postFile = new File("textbin/posts/"+id);
+			if(postFile.exists()) {
+				try {
+					r = DocumentBuilder.readFile("textbin/posts/"+id);
+				} catch (IOException e) {
+					e.printStackTrace();
+					r+="Error reading post";
+				}
+			} else {
+				r = "Invalid post ID";
+			}
+		}
+		
+		return r;
+	}
+}
