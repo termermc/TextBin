387a2a37a9fe790e072dcb8439eaa7e736058ef1
Michael Termer <termer@protonmail.com>
2018-07-24 17:30:16 -0400

    use Posts class instead of internal hashmap

--- src/main/java/net/termer/textbin/Timebomb.java
+++ src/main/java/net/termer/textbin/Timebomb.java
@@ -1,10 +1,6 @@
 package net.termer.textbin;
 
-import java.io.File;
-import java.util.HashMap;
-
 public class Timebomb extends Thread {
-	private HashMap<String,Integer> posts = new HashMap<String,Integer>();
 	private boolean enabled = true;
 	
 	public void run() {
@@ -13,30 +9,18 @@ public class Timebomb extends Thread {
 				sleep(1000*60);
 				
 				// Decrement times
-				String[] keys = posts.keySet().toArray(new String[0]);
-				Integer[] vals = posts.values().toArray(new Integer[0]);
-				
-				for(int i = 0; i < keys.length; i++) {
-					posts.put(keys[i], new Integer(vals[i]-1));
-					if(posts.get(keys[i]).intValue()<1) {
-						new File("textbin/posts/"+keys[i]).delete();
-						posts.remove(keys[i]);
+				for(Post post : Post.getAll()) {
+					post.setExpireTime(post.getExpireTime()-1);
+					
+					// Delete if time is up
+					if(post.getExpireTime()<1) {
+						post.delete();
 					}
 				}
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-	}
-	
-	/**
-	 * Register a post to be deleted
-	 * @param id the post id
-	 * @param time the time before it gets deleted
-	 * @since 1.0
-	 */
-	public void register(String id, int time) {
-		posts.put(id, new Integer(time));
 	}
 	
 	/**
